c5c0b2c5b20be4ab4ff09a6cc35163c6bc73636a
Codeword <saldatejared@gmail.com>
2022-11-16 15:49:09 -0800

    initial commit

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+// Schema to create Post model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      Required:true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      default: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `getTags` that gets the amount of tags associated with an application
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our Application model
+const thought = model('thought', thoughtSchema);
+
+module.exports = thought;
---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type:String,
+      unique:true,
+      required:true,
+      trim:true
+    },
+    email: {
+      type:String,
+      required:true,
+      unique:true,
+      // validate:
+    },
+  
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+    ],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `friendCount` that gets and sets the user's full name
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  })
+  
+const User = model('user', userSchema);
+
+module.exports = User;
